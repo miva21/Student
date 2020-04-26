b2d18d3469bec0d0baf0e946a7840310d511269a
miva21 <64197055+miva21@users.noreply.github.com>
2020-04-26 19:48:30 +0300

    Create Main.java

---
+++ task_1.8/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan=new Scanner(System.in);
+                System.out.println("Введите темпиратуру в Цельсиях   ") ;
+                double TC=scan.nextDouble();
+                double TF=TC*(9.0/5.0)+32;
+                System.out.println("Темпиратура по Фаренгейту равна: "+TF);
+    }
+}
