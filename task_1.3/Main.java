0fcdf08a97d8c1e97437f52c8a2bb08f3bc1dce7
miva21 <64197055+miva21@users.noreply.github.com>
2020-04-26 20:40:02 +0300

    Create Main.java

---
+++ task_1.3/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+public class Main {
+    public static void main(String[] args) {
+        int a=4; //десяток числа
+        int b=7; //еденицы числа
+        System.out.println("Число десятков = "+a);
+        System.out.println("Число едениц = "+b);
+        System.out.println("Итоговое составное число = "+a+b);
+    }
+}
