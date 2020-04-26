079bcb36f41da87989c2226f3c5bf87e29447958
miva21 <64197055+miva21@users.noreply.github.com>
2020-04-26 20:53:16 +0300

    Create Main.java

---
+++ task_1.4/Main.java
@@ -0,0 +1,8 @@
+package com.company;
+public class Main {
+    public static void main(String[] args) {
+        int x=4;        //целочисленное значение
+        double b=x/3.0; //Треть от числа
+        System.out.println("Треть от числа 4 = "+b);
+    }
+}
