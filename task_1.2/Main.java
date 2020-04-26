cd9450cbb7b1a60ebc86947c3c9be2564450f587
miva21 <64197055+miva21@users.noreply.github.com>
2020-04-26 20:38:44 +0300

    Create Main.java

---
+++ task_1.2/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+public class Main {
+    public static void main(String[] args) {
+        double  x=7.5;
+        double  a, b;
+        a=(x*2);
+        b=(x*3);
+        System.out.println("Число вдвое больше 7,5 = "+a);
+        System.out.println("Число втрое больше 7,5 = "+b);
+    }
+}
