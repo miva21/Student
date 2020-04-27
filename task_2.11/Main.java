fca5c2131314892f57dc86912e3dedf090e90e70
miva21 <64197055+miva21@users.noreply.github.com>
2020-04-27 21:06:12 +0300

    Update Main.java

--- task_2.11/Main.java
+++ task_2.11/Main.java
@@ -2,7 +2,7 @@ package com.company;
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Введите ниже число для определения 'Веса Числа'");
+        System.out.println("Введите ниже число для определения 'Четверти Часа'");
         int minut;
         Scanner scan = new Scanner(System.in);
         minut = scan.nextInt();
