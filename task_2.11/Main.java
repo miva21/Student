236ba772e3174becf6bd28a8386ed94257ab66bc
miva21 <64197055+miva21@users.noreply.github.com>
2020-04-27 20:53:40 +0300

    Create Main.java

---
+++ task_2.11/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Введите ниже число для определения 'Веса Числа'");
+        int minut;
+        Scanner scan = new Scanner(System.in);
+        minut = scan.nextInt();
+        if (minut<=15) System.out.println("Первая четверь часа");
+            if (minut>15 && minut<=30) System.out.println("Вторая четверь часа");
+                if (minut>30 && minut<=45) System.out.println("Третья четверь часа");
+                    if (minut>45) System.out.println("Последняя четверь часа");
+        else
+        System.out.println("EnD");
+    }
+}
