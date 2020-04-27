b2a40bf0986213909d975cfd27e89a6873d028b7
miva21 <64197055+miva21@users.noreply.github.com>
2020-04-27 12:57:13 +0300

    Create Main.java

---
+++ task_2.7/Main.java
@@ -0,0 +1,9 @@
+package com.company;
+public class Main {
+    public static void main(String[] args) {
+        int a=0;
+        System.out.println("Система выведет числа от 1 до 50");
+        for (int i=0; i<50; i++) System.out.println(++a);
+        System.out.println("EnD");
+    }
+}
