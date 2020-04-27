056749351c825c7e87e818b65da5d59c55c80601
miva21 <64197055+miva21@users.noreply.github.com>
2020-04-27 13:09:22 +0300

    Create Main.java

---
+++ task_2.8/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+public class Main {
+    public static void main(String[] args) {
+        int a=0;
+        System.out.println("Система выведет числа от 1 до 9 подряд без разделения");
+        for (int i=0; i<9; i++) System.out.print(++a);
+        System.out.println("");
+        System.out.println("EnD");
+    }
+}
