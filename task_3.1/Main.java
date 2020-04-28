1eda82eb5fc5e37c8dd084e74a5a7dac5bd34133
miva21 <64197055+miva21@users.noreply.github.com>
2020-04-28 09:45:06 +0300

    Update Main.java

--- task_3.1/Main.java
+++ task_3.1/Main.java
@@ -1,6 +1,7 @@
 package com.company;
 public class Main {
     public static void main(String[] args) {
+        //Задание 3,1: Дан массив с числами. Проверьте, что в этом массиве есть число 5. Если есть - выведите 'да', а если нет - выведите 'нет'.
         int[] cl={7, 4, 1, 10, 3, 6, 7, 3, 4, 2};
         int t=0;
         for (int i=0; i<cl.length; i++) {
