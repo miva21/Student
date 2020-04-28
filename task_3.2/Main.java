c5d10c616d1332a4307d14df6a3ab6342595af76
miva21 <64197055+miva21@users.noreply.github.com>
2020-04-28 10:49:39 +0300

    Create Main.java

---
+++ task_3.2/Main.java
@@ -0,0 +1,18 @@
+package com.company;
+public class Main {
+    public static void main(String[] args) {
+       /* Задание 3,2: Дан массив с числами.
+       Проверьте, есть ли в нем два одинаковых числа подряд.
+       Если есть - выведите 'да', а если нет - выведите 'нет'.
+        */
+        int ms[]={1,2,4,9,5,6,7,8,7,10};
+        int l=0;
+        for (int i = 0; i < ms.length-1; i++) {
+            if(ms[i]==ms[i+1]) {
+                System.out.print("Да");
+                System.exit(0);
+            }
+        }
+        System.out.print("Нет");
+    }
+}
