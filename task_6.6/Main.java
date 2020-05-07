4f20e0b2240f23773d3431ef3123a677f16ad6fc
miva21 <64197055+miva21@users.noreply.github.com>
2020-05-07 22:00:26 +0300

    Create Main.java

---
+++ task_6.6/Main.java
@@ -0,0 +1,16 @@
+/* Задача 6.6 Перехватить исключение, возникающее при выполнении кода:
+int a = 42 / 0;
+Вывести на экран тип перехваченного исключения.*/
+package com.company;
+public class Main {
+    public static void main(String[] args) {
+        try {
+            int a = 42 / 0;
+            if(a<0) throw new Exception();
+        }
+        catch (Exception ee) {
+              System.out.println(ee.getMessage()+" -- Вы пытаетесь разделить на НОЛЬ");
+              ee.printStackTrace(); // Тип исключения это <ArithmeticException> или </ by zero>?
+        }
+    }
+}
