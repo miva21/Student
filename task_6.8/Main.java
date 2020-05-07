9574d4b1d4516d392645b401d19d591b7c7e91c4
miva21 <64197055+miva21@users.noreply.github.com>
2020-05-07 23:06:59 +0300

    Create Main.java

---
+++ task_6.8/Main.java
@@ -0,0 +1,15 @@
+/* Задача 6,8 Перехватить исключение (и вывести его на экран), указав его тип,
+возникающее при выполнении кода:
+int[] m = new int[2];
+m[8] = 5; */
+package com.company;
+public class Main {
+    public static void main(String[] args) {
+        try {
+            int[] m = new int[2];
+            m[8] = 5;
+        } catch (ArrayIndexOutOfBoundsException sd) {
+            System.out.println(sd.getMessage()+" -- Указано число вне размерности массива!!"); //sd.printStackTrace();
+        }
+    }
+}
