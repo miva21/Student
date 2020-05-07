90f7740cca9367ccccf68f4f754972c2c24a77c6
miva21 <64197055+miva21@users.noreply.github.com>
2020-05-07 08:08:29 +0300

    Create Main.java

---
+++ task_6.2/Main.java
@@ -0,0 +1,36 @@
+/* Задача 6,2 1. Создать public static класс Goose(Гусь).
+2. Создать public static класс Dragon(Дракон).
+3. Унаследовать класс Goose от BigAnimal или SmallAnimal, подумать, какой логически больше подходит.
+4. Унаследовать класс Dragon от BigAnimal или SmallAnimal, подумать, какой логически больше подходит.
+5. В классах Goose и Dragon переопределить метод String getSize(), расширить видимость до максимальной.
+6. В классе Goose метод getSize должен возвращать строку "Гусь маленький, "
++ [getSize родительского класса].
+7. В классе Dragon метод getSize должен возвращать строку "Дракон большой, " + [getSize родительского класса].*/
+package com.company;
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(new Goose().getSize()+" А "+new Dragon().getSize());
+    }
+    public static class Goose extends SmallAnimal {
+        @Override
+        public String getSize() {
+            return "Гусь маленький, " + super.getSize();
+        }
+    }
+    public static class Dragon extends BigAnimal {
+        @Override
+        public String getSize() {
+            return "Дракон большой, " +super.getSize();
+        }
+    }
+    public static class BigAnimal {
+        protected String getSize() {
+            return "как динозавр.";
+        }
+    }
+    public static class SmallAnimal {
+        protected String getSize() {
+            return "как кошка.";
+        }
+    }
+}
